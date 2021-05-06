9d8ecf5f83d37f870d2cc1716fda730ef1d85e11
Eduard <eduarduspanow@gmail.com>
2021-05-06 20:31:00 +0200

    working with tensorflow 2.4.1
    AI seems to be dumb. Training for a long time may help. Upcoming: Trying to train on a simple racetrack

--- tut2.py
+++ tut2.py
@@ -13,17 +13,20 @@ import math
 import tensorflow as tf
 from collections import deque
 from tensorflow.keras.applications.xception import Xception
-from tensorflow.keras.layers import Dense, GlobalAveragePooling2D, Flatten, LSTM, Dense, Dropout
+from tensorflow.keras.layers import Dense, GlobalAveragePooling2D, Input, Concatenate, Conv2D, AveragePooling2D,Activation, Flatten
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras import Model, Sequential
-from tensorflow.keras.callbacks import TensorBoard, EarlyStopping
-import tensorflow.keras.backend as backend
+from tensorflow.keras.callbacks import TensorBoard
+#import keras.backend.tensorflow_backend as backend
 from threading import Thread
 from tqdm import tqdm
 
-from tensorflow.python.keras import backend as K
+from tensorflow.compat.v1 import ConfigProto
+from tensorflow.compat.v1 import InteractiveSession
+
+#from tensorflow.python.keras import backend as K
 #tf.compat.v1.disable_eager_execution()
-#tf.compat.v1.disable_v2_behavior()
+#tf.disable_v2_behavior()
 #tf.compat.v1.executing_eagerly()
 
 
@@ -41,27 +44,50 @@ SHOW_PREVIEW = False
 IM_WIDTH = 640
 IM_HEIGHT = 480
 SECONDS_PER_EPISODE = 10
-REPLAY_MEMORY_SIZE = 5_000
-MIN_REPLAY_MEMORY_SIZE = 1_000
+REPLAY_MEMORY_SIZE = 20_000
+MIN_REPLAY_MEMORY_SIZE = 5_000
 MINIBATCH_SIZE = 16
 PREDICTION_BATCH_SIZE = 1
 TRAINING_BATCH_SIZE = MINIBATCH_SIZE // 4
-UPDATE_TARGET_EVERY = 5
+UPDATE_TARGET_EVERY = 100
 MODEL_NAME = "Xception"
-MEMORY_FRACTION = 0.6 ## SOLVING RTX MEMORY ERROR
+#MEMORY_FRACTION = 0.6 ## SOLVING RTX MEMORY ERROR
 MIN_REWARD = -200
 
 EPISODES = 100
 DISCOUNT = 0.99
 epsilon = 1
-EPSILON_DECAY = 0.955 ## 0.9975 or so later
-MIN_EPSILON = 0.001
+EPSILON_DECAY = 0.99995 ## 0.9975 or so later
+MIN_EPSILON = 0.1
 
 AGGREGATE_STATS_EVERY = 10
 
 # Own Tensorboard class
 
+class ModifiedTensorBoard(TensorBoard):
 
+    # Overriding init to set initial step and writer (we want one log file for all .fit() calls)
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.step = 1
+        self.writer = tf.summary.create_file_writer(self.log_dir)
+    
+    # Overrided, saves logs with our step number
+    # (otherwise every .fit() will start writing from 0th step)
+    def on_epoch_end(self, epoch, logs=None):
+        self.update_stats(**logs)
+
+    # Custom method for saving own metrics
+    # Creates writer, writes custom metrics and closes writer
+    def update_stats(self, **stats):
+        self._write_logs(stats, self.step)
+
+    def update_stats(self, **stats):
+        with self.writer.as_default():
+            for key, value in stats.items():
+                tf.summary.scalar(key, value, step=self.step)
+                self.writer.flush()
+'''
 class ModifiedTensorBoard(TensorBoard):
 
     # Overriding init to set initial step and writer (we want one log file for all .fit() calls)
@@ -92,6 +118,7 @@ class ModifiedTensorBoard(TensorBoard):
     # Creates writer, writes custom metrics and closes writer
     def update_stats(self, **stats):
         self._write_logs(stats, self.step)
+'''
 
 class CarEnv:
     SHOW_CAM = SHOW_PREVIEW
@@ -126,7 +153,7 @@ class CarEnv:
         self.sensor.listen(lambda data: self.process_img(data))
 
         self.vehicle.apply_control(carla.VehicleControl(throttle=0.0, brake=0.0))
-        time.sleep(4)
+        time.sleep(1)
 
         colsensor = self.blueprint_library.find("sensor.other.collision")
         self.colsensor = self.world.spawn_actor(colsensor, transform, attach_to=self.vehicle)
@@ -142,46 +169,46 @@ class CarEnv:
         return self.front_camera
 
     def collision_data(self,event):
-        self.collision.hist.append(event)
+        self.collision_hist.append(event)
 
 
-def process_img(self, image):
-    i = np.array(image.raw_data)
-    #print(i.shape)
-    i2 = i.reshape((self.im_height, self.im_width, 4))
-    i3 = i2[:, :, :3]
-    if self.SHOW_CAM:
-        cv2.imshow( "", i3/255)
-        cv2.waitKey(1)
-    self.front_camera = i3
+    def process_img(self, image):
+        i = np.array(image.raw_data)
+        #print(i.shape)
+        i2 = i.reshape((self.im_height, self.im_width, 4))
+        i3 = i2[:, :, :3]
+        if self.SHOW_CAM:
+            cv2.imshow( "", i3/255)
+            cv2.waitKey(1)
+        self.front_camera = i3
 
-def step(self, action):
-    if action == 0:
-        self.vehicle.apply_control(carla.VehicleControl(throttle=1.0, steer =-1*self.STEER_AMT))
-    elif action == 1:
-        self.vehicle.apply_control(carla.VehicleControl(throttle=1.0, steer=0))
-    elif action ==2:
-        self.vehicle.apply_control(carla.VehicleControl(throttle=1.0, steer=1*self.STEER_AMT))
+    def step(self, action):
+        if action == 0:
+            self.vehicle.apply_control(carla.VehicleControl(throttle=1.0, steer =-1*self.STEER_AMT))
+        elif action == 1:
+            self.vehicle.apply_control(carla.VehicleControl(throttle=1.0, steer=0))
+        elif action ==2:
+            self.vehicle.apply_control(carla.VehicleControl(throttle=1.0, steer=1*self.STEER_AMT))
 
-    v = self.vehicle.get_velocity()
-    kmh = int(3.6 * math.sqrt(v.x**2 + v.y**2 + v.z**2))
+        v = self.vehicle.get_velocity()
+        kmh = int(3.6 * math.sqrt(v.x**2 + v.y**2 + v.z**2))
 
-    if len(self.collision_hist) != 0:
-        done = True
-        reward = -200
+        if len(self.collision_hist) != 0:
+            done = True
+            reward = -200
 
-    elif kmh < 50:
-        done = False
-        reward = -1
+        elif kmh < 20:
+            done = False
+            reward = -1
 
-    else:
-        done = False
-        reward = 1
-    
-    if self.episode_start + SECONDS_PER_EPISODE < time.time():
-        done = True
+        else:
+            done = False
+            reward = 1
+        
+        if self.episode_start + SECONDS_PER_EPISODE < time.time():
+            done = True
 
-    return self.front_camera, reward, done, None
+        return self.front_camera, reward, done, None
 
 class DQNAgent:
     def __init__(self):
@@ -191,10 +218,10 @@ class DQNAgent:
 
         self.replay_memory = deque(maxlen=REPLAY_MEMORY_SIZE)
 
-        #self.tensorboard = ModifiedTensorBoard(log_dir="logs/{MODEL_NAME}-{int(time.time())}")
+        self.tensorboard = ModifiedTensorBoard(log_dir="logs/{MODEL_NAME}-{int(time.time())}")
 
         self.target_update_counter = 0
-        self.graph = tf.compat.v1.get_default_graph()
+        #self.graph = tf.get_default_graph()
 
         self.terminate = False
         self.last_logged_episode = 0
@@ -202,15 +229,54 @@ class DQNAgent:
         self.training_initialized = False
 
     def create_model(self):
-        base_model = Xception(weights=None, include_top=False, input_shape=(IM_HEIGHT, IM_WIDTH, 3))
+       
+       
+        #base_model = Xception(weights=None, include_top=False, input_shape=(IM_HEIGHT, IM_WIDTH, 3))
+#
+ #       x = base_model.output
+  #      x = GlobalAveragePooling2D()(x)
+   #     predictions = Dense(3, activation="linear")(x)
+    #    model = Model(inputs = base_model.input, outputs = predictions)
+     #   model.compile(loss="mse", optimizer=Adam(lr=0.001), metrics=["accuracy"])
+      #  return model
+       
+        model = Sequential()
 
-        x = base_model.output
-        x = GlobalAveragePooling2D()(x)
-        predictions = Dense(3, activation="linear")(x)
-        model = Model(inputs = base_model.input, outputs = predictions)
-        model.compile(loss="mse", optimizer=Adam(lr=0.001), metrics=["accuracy"])
+        model.add(Conv2D(64, (5, 5), input_shape=(
+            IM_HEIGHT, IM_WIDTH, 3), padding='same'))
+
+        model.add(Activation('relu'))
+        model.add(AveragePooling2D(pool_size=(5, 5),
+                strides=(3, 3), padding='same'))
+        model.add(Conv2D(64, (5, 5), padding='same'))
+        model.add(Activation('relu'))
+        model.add(AveragePooling2D(pool_size=(5, 5),
+                strides=(3, 3), padding='same'))
+        model.add(Conv2D(128, (5, 5), padding='same'))
+        model.add(Activation('relu'))
+        model.add(AveragePooling2D(pool_size=(3, 3),
+                strides=(2, 2), padding='same'))
+        model.add(Conv2D(256, (3, 3), padding='same'))
+        model.add(Activation('relu'))
+        model.add(AveragePooling2D(pool_size=(3, 3),
+                strides=(2, 2), padding='same'))
+        model.add(Flatten())
+        #return model.input, model.output
+        ## model_head_hidden_dense
+        inputs = [model.input]
+        x = model.output
+        x = Dense(3, activation='relu')(x)
+        # And finally output (regression) layer
+        predictions = Dense(3, activation='linear')(x)
+        # Create a model
+        model = Model(inputs=inputs, outputs=predictions)
+        model.compile(loss="mse", optimizer=Adam(
+            lr=0.001), metrics=["accuracy"])
         return model
-    
+
+
+
+
     def update_replay_memory(self, transition):
         # transition = (current_state, action, reward, new_state, done)
         self.replay_memory.append(transition)
@@ -222,12 +288,12 @@ class DQNAgent:
         minibatch = random.sample(self.replay_memory, MINIBATCH_SIZE)
 
         current_states = np.array([transition[0] for transition in minibatch])/255
-        with self.graph.as_default():
-            current_qs_list = self.model.predict(current_states, PREDICTION_BATCH_SIZE)
+        #with self.graph.as_default():
+        current_qs_list = self.model.predict(current_states, PREDICTION_BATCH_SIZE)
 
         new_current_states=np.array([transition[0] for transition in minibatch])/255
-        with self.graph.as_default():
-            future_qs_list = self.target_model.predict(new_current_states, PREDICTION_BATCH_SIZE)
+        #with self.graph.as_default():
+        future_qs_list = self.target_model.predict(new_current_states, PREDICTION_BATCH_SIZE)
 
         X = []
         y = []
@@ -246,12 +312,13 @@ class DQNAgent:
             y.append(current_qs)
 
         log_this_step = False
+        
         if self.tensorboard.step > self.last_logged_episode:
             log_this_step = True
             self.last_log_episode = self.tensorboard.step
-
-        with self.graph.as_default():
-            self.model.fit(np.array(x)/255, np.array(y, batch_size=TRAINING_BATCH_SIZE, verbose=0, shuffle=False, callbacks=[self.tensorboard] if log_this_step else None))
+        
+        #with self.graph.as_default():
+        self.model.fit(np.array(x)/255, np.array(y, batch_size=TRAINING_BATCH_SIZE, verbose=0, shuffle=False))
 
         if log_this_step:
             self.target_update_counter += 1
@@ -266,8 +333,8 @@ class DQNAgent:
     def train_in_loop(self):
         X = np.random.uniform(size=(1, IM_HEIGHT, IM_WIDTH,3)).astype(np.float32)
         y = np.random.uniform(size=(1, 3)).astype(np.float32)
-        with self.graph.as_default():
-            self.model.fit(X,y, verbose=False, batch_size=1)
+        #with self.graph.as_default():
+        self.model.fit(X,y, verbose=False, batch_size=1)
 
         self.training_initialized = True
         
@@ -283,12 +350,14 @@ if __name__ == "__main__":
     FPS = 20
     ep_rewards = [-200]
 
-    random.seed(1)
-    np.random.seed(1)
-    tf.random.set_seed(1)
-
-    gpu_options = tf.compat.v1.GPUOptions(per_process_gpu_memory_fraction=MEMORY_FRACTION)
-    K.set_session(tf.compat.v1.Session(config=tf.compat.v1.ConfigProto(gpu_options=gpu_options)))
+    #random.seed(1)
+    #np.random.seed(1)
+    #tf.set_random_seed(1)
+    config = ConfigProto()
+    config.gpu_options.allow_growth = True
+    session = InteractiveSession(config=config)
+    #gpu_options = tf.compat.v1.GPUOptions(per_process_gpu_memory_fraction=MEMORY_FRACTION)
+    #backend.set_session(tf.Session(config=tf.ConfigProto(gpu_options=gpu_options)))
 
     if not os.path.isdir("models"):
         os.makedirs("models")
@@ -330,7 +399,7 @@ if __name__ == "__main__":
             if done:
                 break
 
-        for actor in ev.actor_list:
+        for actor in env.actor_list:
             actor.destroy()
 
             
